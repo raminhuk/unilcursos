dfce0052d4a48b24db7a1522991975459e46f06d
Fabio J Raminhuk <fabioraminhuk@gmail.com>
2024-01-22 23:25:13 -0300

    feat: 🎸 Ajustes page curso

--- app/massoterapia/page.tsx
+++ app/massoterapia/page.tsx
@@ -1,6 +1,7 @@
 import AboutSectionOne from "@/components/About/AboutSectionOne";
 import AboutSectionTwo from "@/components/About/AboutSectionTwo";
 import Breadcrumb from "@/components/Common/Breadcrumb";
+import Video from "@/components/Video";
 
 import { Metadata } from "next";
 import Image from "next/image";
@@ -20,52 +21,38 @@ const Massoterapia = () => {
 
 
                     <div className="flex gap-20 items-start">
-                        <div className="flex flex-col w-7/12 gap-12 dark:text-body-color-dark mb-12 text-base !leading-relaxed text-body-color sm:text-lg md:text-xl">
+                        <div className="flex flex-col w-9/12 gap-12 dark:text-body-color-dark mb-12 text-base !leading-relaxed text-body-color sm:text-lg md:text-xl">
                             <div>
                                 <h1 className="text-3xl font-bold mb-6 text-blue-600">Curso de Massoterapia Profissional</h1>
                                 <div>
-                                    Descubra o Toque que Transforma Vidas: Domine a Massoterapia e Alcance o Inimaginável com as Pontas dos Dedos.
-                                    Desperte a(o) Massoterapeuta Excepcional em Você, Mesmo que esteja Começando do Zero: Junte-se à Comunidade de pessoas que Estão Redefinindo a Arte da Massoterapia.
+                                    <p>Descubra o universo da Massoterapia com nosso curso de alta qualidade. Acesso ilimitado, certificado incluso e aulas detalhadas criadas como uma verdadeira OBRA-PRIMA, moldada para todos, desde iniciantes até profissionais ávidos por inovação.</p>
                                 </div>
+
+                                <Video />
                             </div>
 
                             <div className="flex w-full gap-12">
-                                <div className="flex flex-col w-full gap-12">
-                                    <div className="elementor-column elementor-col-50 elementor-top-column elementor-element elementor-element-13667ee6" data-id="13667ee6" data-element_type="column">
-                                        <div className="elementor-widget-wrap elementor-element-populated">
-                                            <div className="elementor-element elementor-element-2ecc17f2 elementor-widget elementor-widget-image" data-id="2ecc17f2" data-element_type="widget" data-widget_type="image.default">
-                                                <div className="elementor-widget-container">
-                                                    <Image width="463" height="438" src="/images/course/modelo-massoterapia-02.png" alt="Nosso curso é uma OBRA-PRIMA" />
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="elementor-column elementor-col-50 elementor-top-column elementor-element elementor-element-3517476f" data-id="3517476f" data-element_type="column">
-                                        <div className="elementor-widget-wrap elementor-element-populated">
-                                            <div className="elementor-element elementor-element-7b0004f2 elementor-widget elementor-widget-text-editor" data-id="7b0004f2" data-element_type="widget" data-widget_type="text-editor.default">
-                                                <div className="elementor-widget-container">
-                                                    <p>Formação em Massoterapia</p>
-                                                </div>
-                                            </div>
-                                            <div className="elementor-element elementor-element-6db74204 elementor-widget elementor-widget-heading" data-id="6db74204" data-element_type="widget" data-widget_type="heading.default">
-                                                <div className="elementor-widget-container">
-                                                    <h2 className="elementor-heading-title elementor-size-default">Torne-se expert em Massoterapia com aulas detalhadas, acesso por tempo ilimitado e certificado incluso. </h2>
-                                                </div>
-                                            </div>
-                                            <div className="elementor-element elementor-element-6c8b4559 elementor-widget elementor-widget-text-editor" data-id="6c8b4559" data-element_type="widget" data-widget_type="text-editor.default">
-                                                <div className="elementor-widget-container">
-                                                    <p><strong>Nosso curso é uma OBRA-PRIMA</strong>, meticulosamente esculpido para <strong>cada aspirante</strong> ao mundo da Massoterapia!</p><p>Do <strong>absoluto iniciante</strong> ao profissional experiente sedento por <strong>inovação</strong>, cada módulo é uma porta para técnicas inexploradas e sabedoria profunda.</p><p><strong>Muitos de nossos alunos</strong> já estão vivendo a revolução, aplicando as pérolas de conhecimento adquiridas e, como resultado? <strong>Uma enxurrada de clientes encantados!</strong></p><p>Concebido com uma visão de excelência, nosso <strong>Curso de Formação Profissional em Massoterapia</strong> é a chave para que <strong>TODOS</strong> possam não apenas aprender, mas <strong>florescer</strong> na arte e no negócio da Massoterapia. Sua transformação profissional começa aqui e agora!</p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
+                                <div className="flex flex-col w-full">
+                                    
+                                    <h3 className="text-xl font-bold mb-4 ">
+                                    Descubra o Toque que Transforma Vidas: Domine a Massoterapia e Alcance o Inimaginável com as Pontas dos Dedos.
+                                    </h3>
+
+                                    <p className="mb-4">Desvende os segredos da Massoterapia com nosso curso abrangente, projetado para catalisar sua transformação profissional. Acesso ilimitado, certificado garantido e aulas detalhadas moldadas para todos os níveis de habilidade.</p>
+
+                                    <p className="mb-4">Dos fundamentos às técnicas avançadas, cada módulo é uma jornada envolvente, proporcionando não apenas conhecimento, mas uma experiência prática inigualável. Testemunhe sua evolução, assim como muitos de nossos alunos que já estão colhendo os frutos dessa jornada, conquistando clientes encantados e construindo carreiras sólidas.</p>
+
+                                    <p className="mb-4">Nosso compromisso com a excelência e inovação torna nosso Curso de Massoterapia uma oportunidade única para quem busca não apenas aprender, mas dominar a arte da massagem terapêutica. Sua jornada na Massoterapia começa agora — transforme seu futuro profissional conosco!</p>
+
+                                    <Image className="max-w-full h-auto mb-6" src="/images/course/modelo-massoterapia-02.png" alt="Massoterapia" width="463" height="438"/>
 
                                 </div>
                             </div>
                         </div>
                         <div className="w-5/12 p-6 border rounded flex flex-col gap-4 sticky top-32">
                             <div>
-                                <span>de R$ 159,90 Por Apenas</span>
+                                <Image className="w-full h-auto mb-6" src="/images/course/curso-de-massoterapia-profissional.webp" alt="Massoterapia" width={320} height={320}/>
+                                <span className="text-base text-body-color">de <span className="line-through">R$ 159,90</span> por apenas</span>
                                 <div className="flex items-end gap-2">
                                     <h3 className="price text-3xl font-bold text-primary dark:text-white">
                                         <span className="amount">R$ 79,90 </span>
@@ -73,11 +60,19 @@ const Massoterapia = () => {
                                     <span className="text-base text-body-color">ou 9x de R$ 10,02</span>
                                 </div>
                             </div>
-                            <Link href="https://cursodemassoterapia.com/?ref=Q90205632R">
-                                <button className="flex w-full items-center justify-center rounded-sm bg-primary p-3 text-base font-semibold text-white transition duration-300 ease-in-out hover:bg-opacity-80 hover:shadow-signUp">
-                                    Comprar
-                                </button>
-                            </Link>
+
+                            <div className="flex flex-col gap-4">
+                                <Link className="flex w-full" href="https://cursodemassoterapia.com/?ref=Q90205632R">
+                                    <button className="flex w-full items-center justify-center rounded-sm bg-gray-200 p-2 text-base text-gray-500 transition duration-300 ease-in-out hover:bg-opacity-80 hover:shadow-signUp">
+                                        Mais detalhes
+                                    </button>
+                                </Link>
+                                <Link className="flex w-full" href="https://cursodemassoterapia.com/?ref=Q90205632R">
+                                    <button className="flex w-full items-center justify-center rounded-sm bg-primary p-4 text-lg font-semibold text-white transition duration-300 ease-in-out hover:bg-opacity-80 hover:shadow-signUp">
+                                        Comprar
+                                    </button>
+                                </Link>
+                            </div>
                         </div>
                     </div>
                 </div>
--- components/Video/index.tsx
+++ components/Video/index.tsx
@@ -10,23 +10,23 @@ const Video = () => {
   const [isOpen, setOpen] = useState(false);
 
   return (
-    <section className="relative z-10 py-16 md:py-20 lg:py-28">
+    <section className="relative z-10 py-6 md:py-8 lg:py-12">
       <div className="container">
-        <SectionTitle
+        {/* <SectionTitle
           title="We are ready to help"
           paragraph="There are many variations of passages of Lorem Ipsum available but the majority have suffered alteration in some form."
           center
           mb="80px"
-        />
+        /> */}
 
         <div className="-mx-4 flex flex-wrap">
-          <div className="w-full px-4">
+          <div className="w-full">
             <div
-              className="wow fadeInUp mx-auto max-w-[770px] overflow-hidden rounded-md"
+              className="wow fadeInUp mx-auto max-w-full overflow-hidden rounded-md"
               data-wow-delay=".15s"
             >
               <div className="relative aspect-[77/40] items-center justify-center">
-                <Image src="/images/video/video.jpg" alt="video image" fill />
+                <Image src="/images/video/thmbnail-massoterapia-1.png" alt="video image" fill />
                 <div className="absolute right-0 top-0 flex h-full w-full items-center justify-center">
                   <button
                     aria-label="video play button"
@@ -54,11 +54,11 @@ const Video = () => {
         autoplay={true}
         start={true}
         isOpen={isOpen}
-        videoId="L61p2uyiMSo"
+        videoId="LmbCruQ7ugw"
         onClose={() => setOpen(false)}
       />
 
-      <div className="absolute bottom-0 left-0 right-0 z-[-1] h-full w-full bg-[url(/images/video/shape.svg)] bg-cover bg-center bg-no-repeat"></div>
+      {/* <div className="absolute bottom-0 left-0 right-0 z-[-1] h-full w-full bg-[url(/images/video/shape.svg)] bg-cover bg-center bg-no-repeat"></div> */}
     </section>
   );
 };
